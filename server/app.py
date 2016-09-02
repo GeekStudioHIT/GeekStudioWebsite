4b9315f41a403fa43590766263267282d889b4cd
koinhug@126.com <koinhug@126.com>
2016-09-02 21:17:57 +0800

    POST 请求

--- server/app.py
+++ server/app.py
@@ -5,10 +5,12 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello World'
-#
-# @app.route('/joinus', methods=['POST'])
-# def join_us():
-#
+
+@app.route('/joinus', methods=['POST'])
+def join_us():
+    print request.form['name']
+    print request.form['password']
+    return 'welcome'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000, debug=True)
