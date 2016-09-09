31759ea5dfe9260a404ddf96f5b590a18de5422f
王一航 <yihangwang@foxmail.com>
2016-09-09 22:48:33 +0800

    添加统计用户IP以及访问量的功能

---
+++ front-end/index.php
@@ -0,0 +1,494 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+	<meta charset="utf-8">
+
+	<title>GeekStudio</title>
+
+	<meta http-equiv="X-UA-Compatible" content="IE=Edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="keywords" content="">
+	<meta name="description" content="">
+
+	<!-- stylesheets css -->
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/animate.min.css">
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<link rel="stylesheet" href="css/header.css">
+  	<link rel="stylesheet" href="css/et-line-font.css">
+	<link rel="stylesheet" href="css/font-awesome.min.css">
+
+  	<link rel="stylesheet" href="css/vegas.min.css">
+	<link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+<!-- preloader section -->
+<section class="preloader">
+	<div class="sk-circle">
+       <div class="sk-circle1 sk-child"></div>
+       <div class="sk-circle2 sk-child"></div>
+       <div class="sk-circle3 sk-child"></div>
+       <div class="sk-circle4 sk-child"></div>
+       <div class="sk-circle5 sk-child"></div>
+       <div class="sk-circle6 sk-child"></div>
+      <div class="sk-circle7 sk-child"></div>
+       <div class="sk-circle8 sk-child"></div>
+       <div class="sk-circle9 sk-child"></div>
+       <div class="sk-circle10 sk-child"></div>
+       <div class="sk-circle11 sk-child"></div>
+       <div class="sk-circle12 sk-child"></div>
+     </div>
+</section>
+
+<nav class="navbar navbar-default" role="navigation">
+  <div class="container">
+    <div class="navbar-header">
+	  <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+		<span class="sr-only">Toggle navigation</span>
+		<span class="icon-bar"></span>
+		<span class="icon-bar"></span>
+		<span class="icon-bar"></span>
+	  </button>
+
+<!--Replace text with your app name or logo image-->
+      <a class="navbar-brand" href="#">GeekStudio</a>
+    </div>
+    <div class="collapse navbar-collapse navbar-ex1-collapse">
+	  <ul class="nav navbar-nav">
+	  <li><a class="smoothScroll" href="#about">俱乐部简介</a></li>
+	  <li><a class="smoothScroll" href="#awarding">参赛获奖</a></li>
+	  <li><a class="smoothScroll" href="#product">产品展示</a></li>
+	  <li><a class="smoothScroll" href="#contact">Join us</a></li>
+	  </ul>
+    </div>
+  </div>
+</nav>
+
+<!-- home section -->
+<section id="home">
+	<div class="container">
+		<div class="row">
+
+			<div class="col-md-offset-2 col-md-8 col-sm-12">
+				<div class="home-thumb">
+					<h1 class="wow fadeInUp" data-wow-delay="0.4s">Welcome to GeekStudio</h1>
+          			<!-- <h3 class="wow fadeInUp" data-wow-delay="0.6s">We are almost <strong>ready to launch</strong> our <strong>new creative</strong> website!</h3> -->
+          			<a href="#about" class="btn btn-lg btn-default smoothScroll wow fadeInUp hidden-xs" data-wow-delay="0.8s">Let's begin</a>
+        			<!-- <a href="#" data-toggle="modal" data-target="#modal1" class="btn btn-lg btn-success smoothScroll wow fadeInUp" data-wow-delay="1.0s">Notify me!</a> -->
+					<a href="#contact" class="btn btn-lg btn-success smoothScroll wow fadeInUp" data-wow-delay="1.0s">Join us</a>
+				</div>
+			</div>
+
+		</div>
+	</div>		
+</section>
+
+<!-- about section -->
+<section id="about">
+	<div class="container">
+		<div class="row">
+
+      <div class="col-md-6 col-sm-12">
+        <img src="images/about-img.png" class="img-responsive wow fadeInUp" alt="About">
+      </div>
+
+			<div class="col-md-6 col-sm-12">
+				<div class="about-thumb">
+					<div class="section-title">
+						<h1 class="wow fadeIn" data-wow-delay="0.2s">our studio</h1>
+						<h3 class="wow fadeInUp" data-wow-delay="0.4s">GeekStudio is based in HIT</h3>
+					</div>
+					<div class="wow fadeInUp" data-wow-delay="0.6s">
+						<p>想要了解GeekStudio，首先要了解何为Geek。早期的Geek出现在美国俚语当中，现主要指对科学（包括但不限于数学物理化学生物天文IT）抱有狂热爱好的某一类人，他们被翻译为“极客”，或译作“奇客”。在不同时代，年青人的的宣言都一成不变地这样结束：没有人理解我们，社会的价值与我们无关，我们是异样的，因为我们是Geek。不同的时代，Geek有着不同的意义，但他们都用一个共同点：是一群以创新、技术和时尚为生命意义的人，这群人不分性别，不分年龄，共同的战斗在新经济、尖端技术和世界时尚风潮的前线，共同为现代的电子化社会文化做出自己的贡献。</p>
+						<p>Geek无所不在</p>
+					</div>
+				</div>
+			</div>
+
+		</div>
+	</div>
+</section>
+
+<section id="awarding">
+	<div class="container">
+		<div class="section-title">
+			<h1 class="wow fadeInUp text-center" data-wow-delay="0.3s">our award-winning</h1>
+			<p class="wow fadeInUp text-center" data-wow-delay="0.6s">The reason why a great man is great is that he resolves to be a great man.</p>
+		</div>
+		<div class="awarding-container" style="padding-left:200px">
+			<h3 class="wow fadeInUp text-left" data-wow-delay="0.8s">张旭 Peking University Hackthon IBM 企业奖</h3>
+			<h3 class="wow fadeInUp text-left" data-wow-delay="1.0s">朱天成 张旭 触宝 2016 Hackthon 三等奖</h3>
+			<h3 class="wow fadeInUp text-left" data-wow-delay="1.2s">王一航 HIT-CTF 最强单项奖</h3>
+		</div>
+	</div>
+</section>
+
+<section id="product">
+	<div class="container">
+		<div class="section-title">
+			<h1 class="wow fadeInUp text-center" data-wow-delay="0.3s">our product</h1>
+			<p class="wow fadeInUp text-center" data-wow-delay="0.6s">There's only one corner of the universe you can be sure of improving, and that's your own self.</p>
+		</div>
+		
+		<div class="col-md-offset-2 col-md-8 col-sm-12" style="padding-left:100px">
+			<div class="col-xs-6 col-sm-6">
+				<p class="wow fadeInUp text-left" data-wow-delay="0.8s"><a href="https://github.com/GeekStudioHIT">GeekStudio Organization</a></p>
+				<p class="wow fadeInUp text-left" data-wow-delay="0.9s"><a href="https://github.com/voidhug/PinYinCorrectAffterSpeechRecognition">语音识别后纠错方案</a></p>
+				<p class="wow fadeInUp text-left" data-wow-delay="1.0s"><a href="https://github.com/voidhug/AndroidStepHack">Android 计步器</a></p>
+				<p class="wow fadeInUp text-left" data-wow-delay="1.1s"><a href="https://github.com/voidhug/QtBookMIS">基于 Qt 的图书管理系统</a></p>
+				<p class="wow fadeInUp text-left" data-wow-delay="1.2s"><a href="https://github.com/bceportal">BCE 化学计算网站，已集成到卡西欧计算器</a></p>
+				<p class="wow fadeInUp text-left" data-wow-delay="1.3s"><a href="https://github.com/TaikiAkita/NamespaceJS">JavaScript 命名空间构建器 </a></p>
+				<p class="wow fadeInUp text-left" data-wow-delay="1.3s"><a href="https://github.com/qq519043202/Who-set-the-sp">js游戏，谁组的暗牧，游戏图片通过爬虫抓取pixiv排行榜 </a></p>
+				<p class="wow fadeInUp text-left" data-wow-delay="1.3s"><a href="https://github.com/qq519043202/GetOneMillion">Python制作的APP源码。70行代码，MongoDB插入，聚合，Android界面。附赠Python打包APK教程。 </a></p>
+			</div>
+			<div class="col-xs-6 col-sm-6">
+				<p class="wow fadeInUp text-left" data-wow-delay="0.8s"><a href="https://github.com/voidhug/Melbourne">一个好玩的微信机器人</a></p>
+				<p class="wow fadeInUp text-left" data-wow-delay="0.9s"><a href="https://github.com/voidhug/DailyPlanning">个人效率类 App</a></p>
+				<p class="wow fadeInUp text-left" data-wow-delay="1.0s"><a href="https://github.com/ChestnutHeng/KinectToolKit">一个基于 Kinect 的体感游戏</a></p>
+				<p class="wow fadeInUp text-left" data-wow-delay="1.1s"><a href="https://github.com/Zhangxu0501/MyRPC">单机版 RPC 实现</a></p>
+				<p class="wow fadeInUp text-left" data-wow-delay="1.2s"><a href="https://github.com/TaikiAkita/PolymorphicJS">JavaScript 多态函数构建器</a></p>
+				<p class="wow fadeInUp text-left" data-wow-delay="1.3s"><a href="https://github.com/qq519043202/hit-spider">哈工大抢课脚本，爬取全校照片脚本，成绩导出脚本，hitwlan连接脚本。 </a></p>
+				<p class="wow fadeInUp text-left" data-wow-delay="1.3s"><a href="https://github.com/qq519043202/BILI">B站弹幕爬虫 </a></p>
+				<p class="wow fadeInUp text-left" data-wow-delay="1.3s"><a href="https://github.com/qq519043202/BILI_RobFloor">B站抢楼脚本。 </a></p>
+			</div>
+		</div>
+	</div>
+</section>
+
+
+<section id="contact">
+	<div class="container">
+		<div class="row">
+
+			<div class="col-md-offset-2 col-md-8 col-sm-12">
+				<div class="section-title">
+					<h1 class="wow fadeInUp" data-wow-delay="0.3s">sign up</h1>
+					<p class="wow fadeInUp" data-wow-delay="0.6s">Welcome to join us! Welcome to join GeekStudio! Let's do cool things together!</p>
+				</div>
+				<div class="contact-form wow fadeInUp" data-wow-delay="1.0s">
+					<!-- <form id="contact-form"> -->
+					<form id="contact-for" onsubmit="return false;">
+                        <div class="col-md-12 col-sm-6">
+                          	<input name="name" id="name" type="text" class="form-control" placeholder="姓名" required>
+                        </div>
+                        <div class="col-md-12 col-sm-6">
+                          	<input name="specialty" id="specialty" type="text" class="form-control" placeholder="专业" required>
+                        </div>
+						<div class="col-md-12 col-sm-6">
+                          	<input name="stuId" id="stuId" type="text" class="form-control" placeholder="学号" required>
+                        </div>
+                        <div class="col-md-12 col-sm-6">
+                          	<input name="tel" id="tel" type="tel" class="form-control" placeholder="电话" required>
+                        </div>
+						<div class="col-md-12 col-sm-6">
+                          	<input name="qq" id="qq" type="text" class="form-control" placeholder="QQ" required>
+                        </div>
+                        <div class="col-md-12 col-sm-6">
+                          	<input name="mail" id="mail" type="email" class="form-control" placeholder="邮箱" required>
+                        </div>
+           			  	<div class="col-md-12 col-sm-12">
+				   			<textarea name="interests" id="interests" class="form-control" placeholder="感兴趣的技术" rows="6" required></textarea>
+           			  	</div>
+						<div class="col-md-offset-3 col-md-6 col-sm-offset-2 col-sm-8">
+							<input type="submit" class="form-control submit" id="submit" value="Join us">
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<!-- Back top -->
+<a href="#back-top" class="go-top"><i class="fa fa-angle-up"></i></a>
+
+<!-- javscript js -->
+<!-- <script src="js/jquery.js"></script> -->
+<script src="js/jquery.js"></script>
+<script src="js/jquery.validate.min.js"></script>
+<script src="js/messages_zh.js"></script>
+
+<script src="js/bootstrap.min.js"></script>
+
+<script src="js/vegas.min.js"></script>
+
+<script src="js/wow.min.js"></script>
+<script src="js/smoothscroll.js"></script>
+<script src="js/custom.js"></script>
+
+
+<script src="js/bmob-min.js"></script>
+
+<script>
+Bmob.initialize("20c6504d3392fc9bce23df9052ed9cc8", "9cc94cc3adfd5e5adb2632fb8f8f0174");
+
+
+$.validator.setDefaults({
+    submitHandler: function() {
+		var FreshMan = Bmob.Object.extend("FreshMan");
+		var freshman = new FreshMan();
+		// 添加数据，第一个入口参数是Json数据
+		freshman.save({
+		  name: $("#name").val(),
+		  specialty: $("#specialty").val(),
+		  stuId: $("#stuId").val(),
+		  interests: $("#interests").val(),
+		  mail: $("#mail").val(),
+		  qq: $("#qq").val(),
+		  tel: $("#tel").val(),
+		}, {
+		  success: function(freshman) {
+			  location.reload();
+			  $("html,body").animate({scrollTop: top}, 1000);
+		  },
+		  error: function(freshman, error) {
+			  alert("服务器开小差了");
+		  }
+		});
+    }
+});
+
+$().ready(function() {
+    $("#contact-for").validate({
+		rules: {
+			name: "required",
+			specialty: "required",
+			stuId: "required",
+			interests:"required",
+			mail: {
+				required: true,
+				email: true,
+			},
+			qq: {
+				required: true,
+				number: true,
+			},
+			tel: {
+				required: true,
+				checkTel:true,
+			},
+		},
+		messages:{
+			name:"请填写此段信息",
+			specialty:"请填写此段信息",
+			stuId:"请填写此段信息",
+			interests:"请填写此段信息",
+			mail:"请输入正确的E-mail地址",
+			qq:"请填写正确的QQ信息",
+			tel:"请输入正确的11位电话格式",
+		},
+	});
+
+	
+	$.validator.addMethod("checkTel",function(value,element,params){  
+		var checkTel = /^1\d{10}$/;  
+		return this.optional(element)||(checkTel.test(value));  
+	},"请输入正确的11位电话格式");
+	
+});
+
+
+
+</script>
+
+
+<style>
+	ul.timeline {
+		list-style-type: none;
+		background: url("images/product_line.png") repeat-y scroll 120px 0 transparent;
+		margin: 50px 0;
+		padding: 0;
+	}
+				
+	ul.timeline li {
+		position: relative;
+		margin-bottom: 20px;
+	}
+	ul.timeline li .time {
+		position: absolute;
+		width: 90px;
+		text-align: right;
+		left: 0;
+		top: 10px;
+		color: #999;
+	}
+	ul.timeline li .version {
+		position: absolute;
+		width: 290px;
+		text-align: right;
+		left: -200px;
+		<!-- top: 30px; -->
+		top: 0px;
+		font-size: 40px;
+		line-height: 50px;
+		color: #3594cb;
+		overflow: hidden;
+	}
+	ul.timeline li .number {
+		position: absolute;
+		background: url("images/product_dot.png") no-repeat scroll 0 0 transparent;
+		width: 56px;
+		height: 56px;
+		left: 97px;
+		line-height: 56px;
+		text-align: center;
+		color: #fff;
+		font-size: 18px;
+	}
+	ul.timeline li.alt .number {
+		background-image: url("images/product_dot_alt.png");
+	}
+	ul.timeline li .content {
+		margin-left: 180px;
+	}
+	ul.timeline li .content pre {
+		background-color: #3594cb;
+		padding: 20px;
+		color: #fff;
+		font-size: 13px;
+		line-height: 20px;
+	}
+	ul.timeline li.alt .content pre {
+		background-color: #43B1F1;
+	}
+	pre {
+		white-space: pre-wrap;
+		word-wrap: break-word;
+	}
+</style>
+
+</body>
+</html>
+<?php
+	/**
+	 * 功能 : 记录安卓客户端登陆信息
+	 * 格式 :
+	 * 		1. 时间
+	 *		2. IP
+	 *		3. MAC地址
+	 *		4. 安卓客户端版本号
+	 **/
+
+	////将新密码赋值给$password, 必须保证和py文件中的密码一致才可以正常解密
+	$password = "duolaAmeng";
+	#$password = "";
+	////使用在IP138获取的token赋值给$token
+	$token = "0a7f44191a1e6dac8aa31af7357f3797";
+	#$token = "";
+	////
+	$oid = "2244";
+	$mid = "67382";
+
+	//获取POST数据
+	$android_time = $_POST["android_time"];
+	$android_ip = $_POST["android_ip"];
+	$android_time_php = date("Y-m-d h:i:sa",time()+8*3600);
+	$android_mac = $_POST["android_mac"];
+	$android_version = $_POST["android_version"];
+	$android_ip_true = getip_out();
+	$location_of_ip = getLocationOfIP($android_ip_true, $token);
+	$file_name = "AndroidLog.txt";
+
+	//构造日志报表
+	//TOOD 暂时使用txt文本文件 , 以后使用html构造日志报表
+	$android_log_text = "*******************************\n\rTime : \t\t".$android_time.
+														"\n\rTime(PHP) : \t".$android_time_php.
+														"\n\rIP(Android) : \t".$android_ip.
+														"\n\rIP(PHP) : \t".$android_ip_true.
+														"\n\rMAC : \t\t".$android_mac.
+														"\n\rVersion : \t".$android_version.
+														"\n\rLocation : \t".$location_of_ip.
+														"\n\r";
+
+	$android_log_html = "*******************************</br>Time : ".$android_time.
+														"</br>Time(php) : ".$android_time_php.
+														"</br>IP(Android) : ".$android_ip.
+														"</br>IP(PHP) : ".$android_ip_true.
+														"</br>MAC : ".$android_mac.
+														"</br>Version : ".$android_version.
+														"</br>Location : ".$location_of_ip.
+														"</br>";
+
+	//echo $android_log_html;
+
+	//将日志信息追加到文件
+	$android_file = fopen($file_name, "a+") or die("Unable to open file!");
+
+	fwrite($android_file, encrypt($android_log_text, $password)."\n");
+
+	fclose($android_file);
+
+	//加密函数 , 可逆
+	function encrypt($data, $key){
+		$key =   md5($key);
+		$x =   0;
+		$len =   strlen($data);
+		$l =   strlen($key);
+		for($i = 0; $i < $len; $i++){
+			if ($x == $l){
+				$x = 0;
+			}
+			$char .= $key{$x};
+			$x++;
+		}
+		for ($i = 0; $i < $len; $i++){
+			$str .= chr(ord($data{$i}) + (ord($char{$i})) % 256);
+		}
+		return base64_encode($str);
+	}
+
+	//获取访问者IP
+	function getip_out(){ 
+		$ip=false; 
+		if(!empty($_SERVER["HTTP_CLIENT_IP"])){ 
+			$ip = $_SERVER["HTTP_CLIENT_IP"]; 
+		} 
+		if (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) { 
+			$ips教程 = explode (", ", $_SERVER['HTTP_X_FORWARDED_FOR']); 
+			if ($ip) { array_unshift($ips, $ip); $ip = FALSE; } 
+			for ($i = 0; $i < count($ips); $i++) { 
+				if (!eregi ("^(10│172.16│192.168).", $ips[$i])) { 
+					$ip = $ips[$i]; 
+					break; 
+				} 
+			} 
+		} 
+		return ($ip ? $ip : $_SERVER['REMOTE_ADDR']); 
+	}
+
+	//根据IP获取
+	function getLocationOfIP($ip, $token)
+	{
+
+		$request_location_of_ip = "http://api.ip138.com/query/?ip=".$ip."&datatype=jsonp&callback=find&token=".$token;
+		
+		$page_content = curl_file_get_contents($request_location_of_ip);
+
+		//去掉两边的find();
+		$json_content = substr($page_content, 5);
+		$json_content = substr($json_content, 0, -1);
+
+		//echo $json_content;
+
+		$json = json_decode($json_content);
+
+		$data = $json->{'data'};
+
+		$location = $data[0].$data[1].$data[2].$data[3];
+
+		return $location;
+	}
+
+	//获取页面数据
+	function curl_file_get_contents($durl){
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, $durl);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true) ; // 获取数据返回
+		curl_setopt($ch, CURLOPT_BINARYTRANSFER, true) ; // 在启用 CURLOPT_RETURNTRANSFER 时候将获取数据返回
+		$r = curl_exec($ch);
+		curl_close($ch);
+		return $r;
+	}
+?>
